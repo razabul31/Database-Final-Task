6656db7cef2a91cd4c76940ec22677d6a86de58c
IsranLie <twozero2001@gmail.com>
2022-07-30 16:33:45 +0700

    add laporan

--- admin/laporan.php
+++ admin/laporan.php
@@ -1,48 +1,47 @@
 <?php
 $title = 'Laporan';
+require 'functions.php';
 require 'layout-header.php';
 require 'layout-topbar.php';
 require 'layout-sidebar.php';
-?>
 
+$data = get_data($conn, "SELECT * FROM faktur
+            JOIN pemilik ON `faktur`.`NoKTP` = `pemilik`.`NoKTP`
+            JOIN motor ON `faktur`.`NoRangka` = `motor`.`NoRangka`
+            JOIN type ON `motor`.`IdType` = `type`.`IdType`");
+?>
 <div class="container-fluid">
     <div class="card">
         <div class="card-body collapse show">
             <h3 class="page-title text-truncate text-primary font-weight-medium"><?= $title; ?></h3>
         </div>
     </div>
-
     <div class="row">
         <div class="col-12">
             <div class="card">
                 <div class="card-body">
-                    <h4 class="card-title">
-                        <button type="button" class="btn btn-outline-success btn-rounded">
-                            <i class="fas fa-plus"></i> Tambah Laporan
-                        </button>
-                    </h4>
-                    <br>
                     <div class="table-responsive">
                         <table id="table" class="table">
                             <thead>
                                 <tr>
-                                    <th>Name</th>
-                                    <th>Position</th>
-                                    <th>Office</th>
-                                    <th>Age</th>
-                                    <th>Start date</th>
-                                    <th>Salary</th>
+                                    <th>#</th>
+                                    <th>No. Faktur</th>
+                                    <th>Tanggal</th>
+                                    <th>Konsumen</th>
+                                    <th>Total</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr>
-                                    <td>Tiger Nixon</td>
-                                    <td>System Architect</td>
-                                    <td>Edinburgh</td>
-                                    <td>61</td>
-                                    <td>2011/04/25</td>
-                                    <td>$320,800</td>
-                                </tr>
+                                <?php $no = 1;
+                                foreach ($data as $faktur) : ?>
+                                    <tr>
+                                        <td><?= $no++; ?></td>
+                                        <td><?= $faktur['NoFaktur']; ?></td>
+                                        <td><?= $faktur['Tgl']; ?></td>
+                                        <td><?= $faktur['NamaPemilik']; ?></td>
+                                        <td>Rp <?= number_format($faktur['Harga'], 0, ',', '.'); ?></td>
+                                    </tr>
+                                <?php endforeach; ?>
                             </tbody>
                         </table>
                     </div>
